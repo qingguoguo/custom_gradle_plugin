535f7f4d44d496c135e904ce9e2f886b7ee57393
wangqing <wangqing@ximalayaos.com>
2020-01-10 11:44:58 +0800

    feat：仿写fat-aar插件，判断是否有com.android.library插件

--- plugin_src/src/main/groovy/com/qgg/plugin/FatAarPlugin.groovy
+++ plugin_src/src/main/groovy/com/qgg/plugin/FatAarPlugin.groovy
@@ -3,23 +3,33 @@ package com.qgg.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.ProjectConfigurationException
+import org.gradle.api.artifacts.Configuration
 
-class FatAarPlugin implements Plugin<Project> { // 自定义插件实现Plugin接口
+class FatAarPlugin implements Plugin<Project> { // 自定义插件要实现Plugin接口
+    Configuration embedConf
+
     @Override
     void apply(Project project) {
         project.task('fatPluginTask') { // 定义了一个task，名字为CustomPluginTask
             doLast {
                 println "*** fat aar 插件***"
                 checkAndroidPlugin(project)
+                createConfig(project)
             }
         }
     }
 
-    private static boolean checkAndroidPlugin(Project project) {
+    private static void checkAndroidPlugin(Project project) {
         def hasPlugin = project.plugins.hasPlugin("com.android.library")
         if (!hasPlugin) {
             throw new ProjectConfigurationException('fat-aar-plugin must be applied in project that' +
                     ' has android library plugin!', null)
         }
+    }
+
+    private void createConfig(Project project) {
+        embedConf = project.configurations.create('embed')
+        embedConf.visible = false
+        embedConf.transitive = false
     }
 }
